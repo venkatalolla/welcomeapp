bea741f0324272624a00f9043f88a3d7b48bdea4
Ben Hunter <benhunter@Bens-MacBook-Pro-2.local>
2018-06-22 14:09:06 -0400

    kube setup

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 
+# DOCKER_REGISTRY=`docker inspect registry --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'`
+DOCKER_REGISTRY=host.docker.internal
+# REPO_PATH="wherever"
+REPO_PATH=benhunter@host.docker.internal:/Users/benhunter/local-work/dot-net-webinar/welcomeapp
+
 docker run -d --name jenkins \
 	-u root \
 	-p 8080:8080 \
 	-p 50000:50000 \
-	-v /data/jenkins:/var/jenkins_home \
+	-v $HOME/data/jenkins:/var/jenkins_home \
 	-v $HOME/apps:/apps \
 	-v /var/run/docker.sock:/var/run/docker.sock \
+    -e REPO_PATH=$REPO_PATH \
+    -e DOCKER_REGISTRY=$DOCKER_REGISTRY \
 	jenkinsci/blueocean
 
-# docker tag dotnet-app localhost:5000/dotnet-app-v0.1
+# docker run -d -p 5000:5000 --restart=always --name registry registry:2
